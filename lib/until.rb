0fa782b9ef78163dba7ad2e48bb0b49506e88c38
jamescross <crossjames813@gmail.com>
2021-02-16 16:39:59 -0600

    commit tree 54fa57a12cc9cfe498b43e0f14a26b3d888b90c9

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,11 @@
+require 'pry'
 def using_until
   levitation_force = 6
   #your code here
-    
+  #binding.pry
+   until levitation_force == 10
+    puts 'Wingardium Leviosa' 
+    levitation_force += 1
+   end
 end
 
